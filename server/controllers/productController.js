d0dede0cb65927f799ca8d91ce390c461a5a1f7e
Özgür Baserdem <gurr__@hotmail.com>
2021-10-11 16:12:00 +0200

    Added a ternary

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -18,9 +18,9 @@ exports.deleteSingleProduct = async (req, res) => {
 
 exports.addProduct = async (req, res) => {
   const images = [];
-  const image1 = req.body.image1;
-  const image2 = req.body.image2;
-  const image3 = req.body.image3;
+  const image1 = req.body.image1 ? req.body.image1 : "defaultimage.jpg";
+  const image2 = req.body.image2 ? req.body.image2 : "defaultimage2.jpg";
+  const image3 = req.body.image3 ? req.body.image3 : "defaultimage3.jpg";
   images.push(image1, image2, image3);
 
   const newProduct = {
