1356a44cc811be1cc7e4c66df7d147e8f562fb42
“erikaahlstrom” <info@erikasoldh.se>
2021-10-12 10:21:05 +0200

    Add validation when register a user.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,24 +1,42 @@
 const User = require("./../models/UserModel");
 
 exports.createUser = async (req, res) => {
-  const fullAddress = {
-    address: req.body.address,
-    city: req.body.city,
-    zipcode: req.body.zipcode
-  };
+  try {
+  
+    const newUser = { fullName, displayName, email, password, phoneNumber, city, address, zipcode} = req.body;
 
-  const newUser = {
-    fullName: req.body.fullName,
-    displayName: req.body.displayName,
-    email: req.body.email,
-    password: req.body.password,
-    phoneNumber: req.body.phoneNumber,
-    fullAddress: fullAddress
-  };
+
+    // Validations
+
+      if(!fullName || !displayName || !email || !password || !phoneNumber || !address || !city || !zipcode) {
+        return res.status(400).json({errorMessage: "Please fill in all required fields"})
+      }
+
+       if(password.length < 6) {
+        return res.status(400).json({errorMessage: "Please enter a password with at least 6 characters."})
+      }
+
+      const existingUser = await User.findOne({email})
+      if(existingUser) {
+        return res.status(400).json({
+          errorMessage: "This email already exists"
+        })
+      }
+
+      // Hash the password
+
+
+      res.send(newUser);
+
+  } catch(err) {
+
+    console.error("Register:", err);
+    res.status(500).send();
+  }
 
   //   const user = new User(newUser);
   //   user.save().then(user => res.json(user));
-  const user = await new User(newUser);
+/*   const user = await new User(newUser);
   user.save();
-  res.status(200).json(user);
+  res.status(200).json(user); */
 };
