a57e0120a450bf1a73ecbdb3bec98edd2a25097b
“erikaahlstrom” <info@erikasoldh.se>
2021-10-12 15:26:50 +0200

    Add try catch to admin routes

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -4,33 +4,43 @@ const Product = require("./../models/ProductModel");
 // TODO Lista alla ordrar
 
 exports.deleteSingleProduct = async (req, res) => {
-  const productId = req.params.id;
+  try {
+    const productId = req.params.id;
+    await Product.findByIdAndDelete(productId);
 
-  await Product.findByIdAndDelete(productId);
+  } catch(err) {
+      console.error(err)
+      res.status(500).send(err)
+  }
 };
 
 exports.addProduct = async (req, res) => {
-  const images = [];
-  const image1 = req.body.image1 ? req.body.image1 : "defaultimage.jpg";
-  const image2 = req.body.image2 ? req.body.image2 : "defaultimage2.jpg";
-  const image3 = req.body.image3 ? req.body.image3 : "defaultimage3.jpg";
-  images.push(image1, image2, image3);
+  try {
+    const images = [];
+    const image1 = req.body.image1 ? req.body.image1 : "defaultimage.jpg";
+    const image2 = req.body.image2 ? req.body.image2 : "defaultimage2.jpg";
+    const image3 = req.body.image3 ? req.body.image3 : "defaultimage3.jpg";
+    images.push(image1, image2, image3);
+  
+    const newProduct = {
+      title: req.body.title,
+      description: req.body.description,
+      price: req.body.price,
+      category: req.body.category,
+      manufacturer: req.body.manufacturer,
+      thumbnail: req.body.thumbnail,
+      images: images
+    };
+  
+    const product = await new Product(newProduct);
+    product.save();
+    res.status(200).json(product);
 
-  const newProduct = {
-    title: req.body.title,
-    description: req.body.description,
-    price: req.body.price,
-    category: req.body.category,
-    manufacturer: req.body.manufacturer,
-    thumbnail: req.body.thumbnail,
-    images: images
-  };
-
-  const product = await new Product(newProduct);
-  product.save();
-  res.status(200).json(product);
+  } catch(err) {
+    console.error(err)
+    res.status(500).send()
+  }
 };
-
 
 exports.editProduct = async (req, res) => {
   try {
