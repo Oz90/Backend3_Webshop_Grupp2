9ef517ef2cab848558c6692088733a9df2792a03
Flibben <flibben@gmail.com>
2021-10-14 11:38:44 +0200

    Add React-router-dom, route, switch, pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,17 @@ import './App.css';
 import axios from 'axios'
 import { LoginPage } from './Pages/LoginPage';
 import { RegisterPage } from './Pages/RegisterPage';
+import { Route, Switch } from 'react-router-dom'
 
 axios.defaults.withCredentials = true;
 
 function App() {
   return (
     <>
-      <LoginPage />
-      <RegisterPage />
+      <Switch>
+        <Route path="/login" component={LoginPage} />
+        <Route path="/register" component={RegisterPage} />
+      </Switch>
     </>
   );
 }
