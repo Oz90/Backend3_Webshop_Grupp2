7a7d6c2d1f416d181b7edccd0f291b07c9adecf5
“erikaahlstrom” <info@erikasoldh.se>
2021-10-19 16:18:30 +0200

    change cart route url for delete requests.

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const cartController = require('../controllers/cartController');
 
 router.post('/add/:id', cartController.addToCart);
-router.post('/update/:id', cartController.deleteCartItem);
+router.post('/delete/:id', cartController.deleteCartItem);
 router.post('/delete', cartController.deleteCart);
 router.get('/', cartController.getCart);
 
