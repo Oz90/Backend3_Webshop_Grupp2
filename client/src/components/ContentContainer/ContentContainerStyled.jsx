8e2a4a10985493bf84a05b5235c211b5d92db31e
Jimmy Rickardsson <Rickardssonjimmy@gmail.com>
2021-10-19 22:25:03 +0200

    Added styling

--- client/src/components/ContentContainer/ContentContainerStyled.jsx
+++ client/src/components/ContentContainer/ContentContainerStyled.jsx
@@ -4,9 +4,12 @@ const ContentContainerStyled = styled.div`
   display: grid;
   grid-column: 4/10;
   grid-row: 2;
-  height: 3000px;
+  //height: 3000px;
 
-  // background-color: green;
+  grid-template-columns: repeat(3,1fr);
+  grid-gap: 5px;
+
+   //background-color: #c1eec1;
 `;
 
 export { ContentContainerStyled };
