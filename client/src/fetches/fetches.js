4371383cf3b334526f8b091a17eed061960b81d0
“erikaahlstrom” <info@erikasoldh.se>
2021-10-20 11:05:23 +0200

    Add getUser and getUserOrders fetches.

--- client/src/fetches/fetches.js
+++ client/src/fetches/fetches.js
@@ -8,6 +8,9 @@ const url = axios.create({ baseURL: 'http://localhost:5000/' });
 export const loginUser = (payload) => url.post('/users/login', payload);
 export const registerUser = (payload) => url.post('/users/register', payload);
 export const logoutUser = () => url.get('users/logout');
+export const getUser = () => url.get('/users');
+export const getUserOrders = () => url.get('/orders/user');
+
 
 // const fetches = {
 //   loginUser,
