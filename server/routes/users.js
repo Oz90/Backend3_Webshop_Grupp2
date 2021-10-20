9d7a1f793dec7cc75246c28d50946211458f3aee
“erikaahlstrom” <info@erikasoldh.se>
2021-10-20 11:03:01 +0200

    Change route order.

--- server/routes/users.js
+++ server/routes/users.js
@@ -5,12 +5,10 @@ const userController = require('../controllers/userController');
 
 router.post('/register', userController.registerUser);
 router.post('/update/:id', userController.updateUser);
-
 router.post('/login', userController.loginUser);
-
 router.get('/logout', userController.logoutUser);
-
 router.get('/loggedinuser', userController.loggedInUser);
 router.get('/loggedinadmin', userController.loggedInAdmin);
+router.get('/', userController.getUser);
 
 module.exports = router;
