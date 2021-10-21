91f1db99dfb67d1abaea5a7b6ebb4d1166ab8f5f
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-10-21 10:06:12 +0200

    added carousel to detail page

--- client/src/Pages/DetailPage.jsx
+++ client/src/Pages/DetailPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { CarouselComp } from '../components/Carousel/CarouselComp'
 
 export const DetailPage = () => {
+    
     return (
         <div>
             <h1>Detail Page</h1>
+            <CarouselComp/>
         </div>
     )
 }
