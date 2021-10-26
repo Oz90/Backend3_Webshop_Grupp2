904374fff7b95c90e6f4dbaecc875fae041b196b
“erikaahlstrom” <info@erikasoldh.se>
2021-10-26 15:36:43 +0200

    Change main grid layout.

--- client/src/components/ContentContainer/ContentContainerStyled.jsx
+++ client/src/components/ContentContainer/ContentContainerStyled.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const ContentContainerStyled = styled.div`
   display: grid;
-  grid-column: 4/10;
+  grid-column: 3/10;
   grid-row: 2;
   //height: 3000px;
   overflow: scroll;
