43650b2e04891f3b0bb2d9867d95c7fa0512159f
“erikaahlstrom” <info@erikasoldh.se>
2021-10-26 15:37:05 +0200

    Print products on prder page.

--- client/src/Pages/UserPageOrderDetails.jsx
+++ client/src/Pages/UserPageOrderDetails.jsx
@@ -1,14 +1,20 @@
 import React, { useState, useEffect } from 'react'
 import { useParams } from "react-router-dom"
-import { getSingleOrder } from '../fetches/fetches';
+import { getSingleOrder, getSingleProduct } from '../fetches/fetches';
 import Moment from 'react-moment'
 import {
     ListContainerStyled,
     TextStyled,
     ListStyled,
-    ButtonStyled,
+    CardStyled,
     TitleStyled
 } from '../components/Details/DetailsStyled'
+
+import {
+    CartCardContainerBorder,
+    CartCardImage,
+    CartCardInfo,
+} from '../components/Cart/CartCardStyled';
 
 
 
@@ -16,14 +22,24 @@ export default function UserPageOrderDetails() {
 
     const { id } = useParams();
     const [order, setOrder] = useState([]);
+    const [products, setProducts] = useState([]);
 
     useEffect(() => {
-        getSingleOrder(id).then(res => setOrder(res.data))
-        
-        
-    }, [])
+        setOrder([]);
+        getSingleOrder(id)
+            .then(res => setOrder(res.data))
+                
+    }, []);
 
-    order ? console.log(order) : console.log("bajs")
+    useEffect(() => {
+        setProducts([]);
+        order?.products?.map((item) => {
+            getSingleProduct(item._id)
+                .then(res => setProducts(products => [...products, { ...res.data, amount: item.amount }]));
+        });
+
+    }, [order]); 
+
 
     return (
         <>
@@ -31,10 +47,6 @@ export default function UserPageOrderDetails() {
             <ListStyled>
                 <TitleStyled>Shipped</TitleStyled>
                 <TextStyled>{order?.isShipped ? "Yes" : "No"}</TextStyled>
-                <TitleStyled>Shipping Cost</TitleStyled>
-                <TextStyled>{order?.shippingCost}</TextStyled>
-                <TitleStyled>Total Cost (Without shipping)</TitleStyled>
-                <TextStyled>{order?.totalSum}</TextStyled>
                 <TitleStyled>Order Date </TitleStyled>
                     <TextStyled>
                         <Moment format="YYYY-MM-DD">{order.createdAt}</Moment>
@@ -45,6 +57,26 @@ export default function UserPageOrderDetails() {
                 <TextStyled>{order?.shippingAddress?.zipcode}</TextStyled>
                 <TitleStyled>City</TitleStyled>
                 <TextStyled>{order?.shippingAddress?.city}</TextStyled>
+                <TitleStyled>Products</TitleStyled>
+                {
+                    products?.map((product) => {
+                            return <>
+                                <CartCardContainerBorder>
+                                    <CartCardImage src={product.thumbnail} alt={product.title}/>
+                                    <CartCardInfo>
+
+                                    <p>Title: {product.title}</p>
+                                    <p>Amount: {product.amount} </p>
+                                    <p>price: {product.price} SEK</p>
+                                    </CartCardInfo>
+                                </CartCardContainerBorder>
+                            </>       
+                    })
+                }
+                <TitleStyled>Shipping Cost</TitleStyled>
+                <TextStyled>{order?.shippingCost} SEK</TextStyled>
+                <TitleStyled>Total Cost</TitleStyled>
+                    <TextStyled>{order?.totalSum + order?.shippingCost} SEK</TextStyled>
             </ListStyled>
         </ListContainerStyled>
         </>
