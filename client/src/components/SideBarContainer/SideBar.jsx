cf81ec192524efa83d410de87de13e0a6e5b7f1e
“erikaahlstrom” <info@erikasoldh.se>
2021-10-26 21:25:47 +0200

    Add styling for sidebar.

--- client/src/components/SideBarContainer/SideBar.jsx
+++ client/src/components/SideBarContainer/SideBar.jsx
@@ -1,8 +1,61 @@
 import React, { useState, useEffect } from 'react'
 import { SideBarStyled } from './SideBarStyled';
 import { getAllProducts } from '../../fetches/fetches';
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import styled from 'styled-components';
+import { HiSearch } from 'react-icons/hi';
 
+const StyledLink = styled.a`
+        background-color: inherit;
+        text-decoration: none;
+        border: none;
+        cursor: pointer;
+        color: black;
+        font-size: 1rem;
+        font-weight: 300;
+        letter-spacing: 1.4px;
+        
+        `
+const StyledUl = styled.ul`
+    display: flex;
+    align-items:center;
+    flex-flow: column;
+
+    `
+
+const StyledLi = styled.li`
+    margin-top: 2rem;
+    list-style: none;
+    transition: all .2s ease-in-out;
+    color: black;
+        &:hover {
+            transform: scale(1.1);
+            font-weight: 500;
+        }
+    `
+
+const SearchContainer = styled.div`
+    border: 1px solid black;
+    border-radius: 50px;
+    height: 2rem;
+    display: flex;
+    align-items: center;
+    margin:5rem 1rem 0 1rem;
+    padding: 5px;
+`
+const Input = styled.input`
+    border: none;
+    background: none;
+    :active {
+        background-color:red;
+    }
+    :focus {
+        outline: none;
+        background: none;
+        height: 100%;
+        weight: 100%
+    }
+`
 export const SideBar = () => {
     const [products, setProducts] = useState([]);
 
@@ -13,16 +66,21 @@ export const SideBar = () => {
     const allUniqueCategories = [...new Set(products.map(products => products.category))]
     // console.log(allUniqueCategories);
 
-
     return (
         <SideBarStyled>
-            <ul className="sidebarListItems">
+            <StyledUl className="sidebarListItems">
+            <SearchContainer>
+                <HiSearch /> <Input />
+            </SearchContainer>
                 {allUniqueCategories.map((category, index) => {
-                    return (<Link className="reactLink" to={`/products/${category}`} >
-                        <li key={index}><p className="sidebarLinks">{category}</p></li>
-                    </Link>);
+                    return (
+                    <StyledLink>
+                            <Link className="reactLink" to={`/products/${category}`} >
+                                <StyledLi key={index}>{category}</StyledLi>
+                            </Link>
+                    </StyledLink> );
                 })}
-            </ul>
+            </StyledUl>
         </SideBarStyled >
     )
 }
--- client/src/components/SideBarContainer/SideBarStyled.jsx
+++ client/src/components/SideBarContainer/SideBarStyled.jsx
@@ -1,11 +1,13 @@
 import styled from 'styled-components';
 
+
+
 export const SideBarStyled = styled.div`
   display: grid;
   grid-column: 1/3;
   grid-row: 2;
 
-  background-color: #f8f8c7;
+  background-color: white;
 `;
 
 // export default { SideBarStyled };
