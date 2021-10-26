3a61f93d62c62f79e5b8c7b13380fff3d08f6c46
Jimmy Rickardsson <Rickardssonjimmy@gmail.com>
2021-10-26 10:00:31 +0200

    added a serch function

--- client/src/components/SideBarContainer/SideBar.jsx
+++ client/src/components/SideBarContainer/SideBar.jsx
@@ -1,28 +1,54 @@
-import React, { useState, useEffect } from 'react'
-import { SideBarStyled } from './SideBarStyled';
-import { getAllProducts } from '../../fetches/fetches';
+import React, { useState, useEffect } from "react"
+import { useHistory } from "react-router-dom"
+import { SideBarStyled } from "./SideBarStyled"
+import { getAllProducts } from "../../fetches/fetches"
 import { Link } from "react-router-dom"
+import { InputStyled } from "../Form/FormStyled"
 
 export const SideBar = () => {
-    const [products, setProducts] = useState([]);
+  const [searchItem, setSearchItem] = useState()
+  const [products, setProducts] = useState([])
 
-    useEffect(() => {
-        getAllProducts().then(res => setProducts(res.data))
-    }, []);
+  const history = useHistory()
+  useEffect(() => {
+    getAllProducts().then((res) => setProducts(res.data))
+  }, [])
 
-    const allUniqueCategories = [...new Set(products.map(products => products.category))]
-    // console.log(allUniqueCategories);
+  const allUniqueCategories = [
+    ...new Set(products.map((products) => products.category)),
+  ]
+  // console.log(allUniqueCategories);
 
+  const handleOnChange = (e) => {
+    for (let i = 0; i < products.length; i++) {
+      if (e.target.value == products[i].title) {
+        setSearchItem(e.target.value)
+        history.push(`/detailpage/${products[i]._id}`)
+      } else {
+        //setSearchItem("test")
+      }
+    }
+    //console.log(e.target.value)
+  }
 
-    return (
-        <SideBarStyled>
-            <ul className="sidebarListItems">
-                {allUniqueCategories.map((category, index) => {
-                    return (<Link className="reactLink" to={`/products/${category}`} >
-                        <li key={index}><p className="sidebarLinks">{category}</p></li>
-                    </Link>);
-                })}
-            </ul>
-        </SideBarStyled >
-    )
+  useEffect(() => {
+    console.log(searchItem)
+  }, [searchItem])
+
+  return (
+    <SideBarStyled>
+      <ul className="sidebarListItems">
+        <InputStyled onChange={handleOnChange}></InputStyled>
+        {allUniqueCategories.map((category, index) => {
+          return (
+            <Link className="reactLink" to={`/products/${category}`}>
+              <li key={index}>
+                <p className="sidebarLinks">{category}</p>
+              </li>
+            </Link>
+          )
+        })}
+      </ul>
+    </SideBarStyled>
+  )
 }
