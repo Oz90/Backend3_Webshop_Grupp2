8832e46e7c9f31165343d3f4f7d971ab1fe8e17c
“erikaahlstrom” <info@erikasoldh.se>
2021-10-27 10:32:28 +0200

    Change main styling grid layout.

--- client/src/components/ContentContainer/ContentContainerStyled.jsx
+++ client/src/components/ContentContainer/ContentContainerStyled.jsx
@@ -2,10 +2,11 @@ import styled from 'styled-components';
 
 const ContentContainerStyled = styled.div`
   display: grid;
-  grid-column: 3/10;
+  grid-column: 3/11;
   grid-row: 2;
   //height: 3000px;
   overflow: scroll;
+  padding: 2rem 4rem 0 0;
 
   grid-template-columns: repeat(3,1fr);
   grid-gap: 5px;
