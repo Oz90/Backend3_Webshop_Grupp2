d1e0c5e7be17c141b8d283a19827fe081c354ac6
“erikaahlstrom” <info@erikasoldh.se>
2021-10-27 17:33:32 +0200

    Clean up code.

--- client/src/components/ContentContainer/ContentContainerStyled.jsx
+++ client/src/components/ContentContainer/ContentContainerStyled.jsx
@@ -4,14 +4,10 @@ const ContentContainerStyled = styled.div`
   display: grid;
   grid-column: 3/11;
   grid-row: 2;
-  //height: 3000px;
   overflow: scroll;
   padding: 2rem 4rem 0 0;
-
   grid-template-columns: repeat(3,1fr);
   grid-gap: 5px;
-
-   //background-color: #c1eec1;
 `;
 
 export { ContentContainerStyled };
