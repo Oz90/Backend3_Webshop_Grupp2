44ce1b9d34208e924d687b4e610faa0e21e5cdef
“erikaahlstrom” <info@erikasoldh.se>
2021-10-27 18:27:17 +0200

    Add component for general text styling.

---
+++ client/src/components/Texts/TextsStyled.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+import Colors from '../../styleAssets/Colors.js';
+
+const HeaderOne = styled.h1`
+    font-size: 3rem;
+    font-weight: 800;
+    letter-spacing: 1.2px;
+    color: ${Colors.primary};
+    margin: 2rem 0;
+    
+`;
+
+
+export {
+    HeaderOne
+};
