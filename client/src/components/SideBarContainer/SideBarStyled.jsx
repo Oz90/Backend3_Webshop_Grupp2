8dfd308cc18d739311516205f80310c457377a53
“erikaahlstrom” <info@erikasoldh.se>
2021-10-27 14:57:31 +0200

    Imort colors.

--- client/src/components/SideBarContainer/SideBarStyled.jsx
+++ client/src/components/SideBarContainer/SideBarStyled.jsx
@@ -1,13 +1,72 @@
 import styled from 'styled-components';
+import Colors from '../../styleAssets/Colors.js';
 
+const StyledLink = styled.a`
+  background-color: inherit;
+  text-decoration: none;
+  border: none;
+  cursor: pointer;
+  color: ${Colors.primary};
+  font-size: 1rem;
+  font-weight: 500;
+  letter-spacing: 1.2px;
+  
+  `
+const StyledUl = styled.ul`
+  display: flex;
+  align-items:center;
+  flex-flow: column;
 
+  `
 
-export const SideBarStyled = styled.div`
+const StyledLi = styled.li`
+  margin-top: 2rem;
+  list-style: none;
+  transition: all .2s ease-in-out;
+  color: ${Colors.primary};
+      &:hover {
+          transform: scale(1.1);
+          font-weight: 600;
+      }
+  `
+
+const SearchContainer = styled.div`
+  border: 3px solid ${Colors.primary};
+  display: flex;
+  align-items: center;
+  margin: 2rem 1rem 0 1rem;
+  padding: .5rem;
+  svg {
+    color: ${Colors.primary};
+    margin-right: .3rem;
+  }
+`
+const Input = styled.input`
+  border: none;
+  background: none;
+  color: ${Colors.primary};
+  font-size: 1rem;
+  :focus {
+      outline: none;
+      background: none;
+      height: 100%;
+  }
+  ::placeholder {
+    color: ${Colors.primary};
+  }
+`
+
+const SideBarStyled = styled.div`
   display: grid;
   grid-column: 1/3;
   grid-row: 2;
-
-  background-color: white;
 `;
 
-// export default { SideBarStyled };
+export {  
+  SideBarStyled, 
+  Input, 
+  SearchContainer,
+  StyledLink,
+  StyledUl,
+  StyledLi
+ };
