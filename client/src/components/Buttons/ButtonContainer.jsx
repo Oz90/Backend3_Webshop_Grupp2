dff4eb55d460e60759942a0763bea2f4f32de03e
“erikaahlstrom” <info@erikasoldh.se>
2021-10-27 17:57:03 +0200

    Create Button container.

---
+++ client/src/components/Buttons/ButtonContainer.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import Colors from '../../styleAssets/Colors';
+
+const ButtonContainer = styled.div`
+    margin-bottom: 2rem;
+    button {
+        margin-right: 1rem;
+    }
+`;
+
+
+export {
+    ButtonContainer,
+};
