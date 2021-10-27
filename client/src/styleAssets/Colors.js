e9a3f5afe9b70eb57e03cb40f10f5cebb58c5a17
“erikaahlstrom” <info@erikasoldh.se>
2021-10-27 16:39:18 +0200

    Update colors.

--- client/src/styleAssets/Colors.js
+++ client/src/styleAssets/Colors.js
@@ -2,7 +2,7 @@ const Colors = {
     primary:'#998bf7',
     secondary: '#7d6afc',
     danger: '#ff6969',
-    success: '#5effaf',
+    success: '#0ecf7b',
     white: '#fff',
     black: '#000', 
 }
