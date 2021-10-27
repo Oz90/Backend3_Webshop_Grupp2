43757a497eb0f683c169769a6da779958d8e5f2c
“erikaahlstrom” <info@erikasoldh.se>
2021-10-27 14:36:17 +0200

    Create separate file for colors.

---
+++ client/src/styleAssets/Colors.js
@@ -0,0 +1,10 @@
+const Colors = {
+    primary:'#998bf7',
+    secondary: '#7d6afc',
+    danger: '#ff6969',
+    success: '#5effaf',
+    white: '#fff',
+    black: '#000', 
+}
+
+module.exports = Colors;
