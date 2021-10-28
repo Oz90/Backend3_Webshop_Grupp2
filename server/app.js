0024bd6cf882892e2a507c1274bb4661766bda54
Özgür Baserdem <gurr__@hotmail.com>
2021-10-28 16:14:38 +0200

    Added console.log for env variables

--- server/app.js
+++ server/app.js
@@ -23,6 +23,11 @@ const cartRouter = require('./routes/cart');
 
 const app = express();
 
+console.log(process.env.JWT_SECRET_ADMIN)
+console.log(process.env.JWT_SECRET_USER)
+console.log(process.env.ATLAS_URI)
+console.log(process.env.CLIENT_URL)
+
 // Middlewares
 app.use(
   cors({
