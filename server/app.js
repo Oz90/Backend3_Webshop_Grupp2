c986dd992bac10837170b8bbc16e8bfe08ee0aa8
Özgür Baserdem <gurr__@hotmail.com>
2021-10-28 22:29:46 +0200

    Added res.header

--- server/app.js
+++ server/app.js
@@ -44,6 +44,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Credentials', true)
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
