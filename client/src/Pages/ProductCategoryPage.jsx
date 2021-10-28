9742cd47d10294e7d6efb54c6babac64ec32267a
Jimmy Rickardsson <Rickardssonjimmy@gmail.com>
2021-10-28 14:14:32 +0200

    Refactored styling to styled components

--- client/src/Pages/ProductCategoryPage.jsx
+++ client/src/Pages/ProductCategoryPage.jsx
@@ -1,50 +1,53 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react"
 import { useParams, Link } from "react-router-dom"
-import { getAllProducts } from '../fetches/fetches';
+import { getAllProducts } from "../fetches/fetches"
+import {
+  StyledCard,
+  StyledImgDiv,
+  StyledThumbnail,
+  StyledProductTitle,
+  StyledTitleAndPrice,
+} from "../components/StylingProductCategoryPage/StyledProductCategoryPage"
 
 export const ProductCategoryPage = () => {
-    let { id } = useParams();
-    console.log(id);
-    const [products, setProducts] = useState([]);
-    const [filteredProducts, setFilteredProducts] = useState([]);
+  let { id } = useParams()
+  console.log(id)
+  const [products, setProducts] = useState([])
+  const [filteredProducts, setFilteredProducts] = useState([])
 
+  useEffect(() => {
+    getAllProducts().then((res) => setProducts(res.data))
+  }, [])
 
-    useEffect(() => {
-        getAllProducts().then(res => setProducts(res.data))
-    }, []);
+  useEffect(() => {
+    filterProducts(id)
+    console.log(products)
+  }, [products, id])
 
-    useEffect(() => {
-        filterProducts(id);
-        console.log(products);
-    }, [products, id])
+  const filterProducts = (id) => {
+    const filteredArray = products.filter((product) => product.category === id)
+    setFilteredProducts(filteredArray)
+  }
 
-    const filterProducts = id => {
-        const filteredArray = products.filter((product) => product.category === id);
-        setFilteredProducts(filteredArray);
-
-    }
-
-
-
-    return (
-        <>
-            {filteredProducts.map((product, index) => {
-                return (
-                    <>
-                        <Link to={`/detailpage/${product._id}`}>
-                            <div className="card">
-                                <div className="imgDiv">
-                                <img class="thumbnail" src={product.thumbnail} alt="" />
-                                </div>
-                                <div className="titleAndPrice">
-                                    <h2 class="productTitle">{product.title}</h2>
-                                    <p className="productPrice">Price {product.price} Kr</p>
-                                </div>
-                            </div>
-                        </Link>
-                    </>
-                )
-            })}
-        </>
-    );
-};
+  return (
+    <>
+      {filteredProducts.map((product, index) => {
+        return (
+          <>
+            <Link to={`/detailpage/${product._id}`}>
+              <StyledCard>
+                <StyledImgDiv>
+                  <StyledThumbnail  src={product.thumbnail} alt="" />
+                </StyledImgDiv>
+                <StyledTitleAndPrice>
+                  <StyledProductTitle >{product.title}</StyledProductTitle>
+                  <p>Price {product.price} Kr</p>
+                </StyledTitleAndPrice>
+              </StyledCard>
+            </Link>
+          </>
+        )
+      })}
+    </>
+  )
+}
--- client/src/Pages/ProductPage.jsx
+++ client/src/Pages/ProductPage.jsx
@@ -1,42 +1,49 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react"
 import { Link } from "react-router-dom"
-import { getAllProducts } from '../fetches/fetches';
-import { Container } from '../components/ProductPage/ProductPageStyled'
+import { getAllProducts } from "../fetches/fetches"
+import { Container } from "../components/ProductPage/ProductPageStyled"
+import {
+  StyledCard,
+  StyledImgDiv,
+  StyledThumbnail,
+  StyledProductTitle,
+  StyledTitleAndPrice,
+} from "../components/StylingProductCategoryPage/StyledProductCategoryPage"
 
 export const ProductPage = () => {
-    const [products, setProducts] = useState([]);
-    const [shuffledProducts, setShuffledProducts] = useState([]);
+  const [products, setProducts] = useState([])
+  const [shuffledProducts, setShuffledProducts] = useState([])
 
-    useEffect(() => {
-        getAllProducts().then(res => setProducts(res.data))
-    }, []);
+  useEffect(() => {
+    getAllProducts().then((res) => setProducts(res.data))
+  }, [])
 
-    function shuffle(array) {
-        return array.sort(() => Math.random() - 0.5);
-    }
+  function shuffle(array) {
+    return array.sort(() => Math.random() - 0.5)
+  }
 
-    useEffect(() => {
-        const shuffledArray = shuffle(products);
-        setShuffledProducts(shuffledArray);
-    }, [products]);
+  useEffect(() => {
+    const shuffledArray = shuffle(products)
+    setShuffledProducts(shuffledArray)
+  }, [products])
 
-    return (
-        <> 
-            {/* <Container> */}
-            {shuffledProducts.map((product, index) => {
-                return <Link to={`/detailpage/${product._id}`}>
-                    <div className="card">
-                        <div className="imgDiv">
-                            <img class="thumbnail" src={product.thumbnail} alt="" />
-                        </div>
-                        <div className="titleAndPrice">
-                            <h2 class="productTitle">{product.title}</h2>
-                            <p className="productPrice">Price {product.price} Kr</p>
-                        </div>
-                    </div>
-                </Link>
-            })}
-           {/*  </Container> */}
-        </>
-    );
-};
+  return (
+    <>
+      {shuffledProducts.map((product, index) => {
+        return (
+          <Link to={`/detailpage/${product._id}`}>
+            <StyledCard>
+              <StyledImgDiv>
+                <StyledThumbnail src={product.thumbnail} alt="" />
+              </StyledImgDiv>
+              <StyledTitleAndPrice>
+                <StyledProductTitle>{product.title}</StyledProductTitle>
+                <p>Price {product.price} Kr</p>
+              </StyledTitleAndPrice>
+            </StyledCard>
+          </Link>
+        )
+      })}
+    </>
+  )
+}
