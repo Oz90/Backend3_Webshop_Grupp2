f2ad73fa1c1f3ccd13534bfd6a9db0ca102130d4
Özgür Baserdem <gurr__@hotmail.com>
2021-10-28 17:48:40 +0200

    Tried resetting code with urls on axios and cors

--- server/app.js
+++ server/app.js
@@ -26,7 +26,7 @@ const app = express();
 // Middlewares
 app.use(
   cors({
-    origin: ['http://aqueous-castle-08428.herokuapp.com/'],
+    origin: ['http://aqueous-castle-08428.herokuapp.com/', 'http://localhost:3000', 'http://localhost:3001'],
     credentials: true,
   }),
 );
