aafa4a0fd4abca2f9283168d71c659450a2b71d1
Özgür Baserdem <gurr__@hotmail.com>
2021-10-28 21:51:54 +0200

    Trying to add access controll

--- server/app.js
+++ server/app.js
@@ -35,11 +35,18 @@ app.use(
     credentials: true,
   }),
 );
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
