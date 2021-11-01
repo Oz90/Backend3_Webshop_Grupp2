1a3993d844dcdf7b1913e81882f36a2a91721693
“erikaahlstrom” <info@erikasoldh.se>
2021-11-01 16:45:16 +0100

    Include inputs in buttoncontainer component.

--- client/src/components/Buttons/ButtonContainer.jsx
+++ client/src/components/Buttons/ButtonContainer.jsx
@@ -3,13 +3,13 @@ import Colors from '../../styleAssets/Colors';
 
 const ButtonContainer = styled.div`
     margin-bottom: 2rem;
-    button {
+    button, input {
         margin-right: 1rem;
     }
 `;
 const ButtonContainerBottom = styled.div`
     margin-top: 2rem;
-    button {
+    button, input {
         margin-right: 1rem;
     }
 `;
